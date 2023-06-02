14f1f78549d636232aa7342b50d91ae7dda8c047
R-Gutu <56069744+R-Gutu@users.noreply.github.com>
2023-06-02 14:55:09 +0300

    fix

--- skylinesdk/src/main/java/com/skline/sdksupport/MainClass.kt
+++ skylinesdk/src/main/java/com/skline/sdksupport/MainClass.kt
@@ -4,12 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.provider.Settings
 import android.util.Log
-import com.appsflyer.AppsFlyerConversionListener
-import com.appsflyer.AppsFlyerLib
-import com.facebook.FacebookSdk
-import com.facebook.applinks.AppLinkData
-import com.google.android.gms.ads.identifier.AdvertisingIdClient
-import com.onesignal.OneSignal
+//import com.appsflyer.AppsFlyerConversionListener
+//import com.appsflyer.AppsFlyerLib
+//import com.facebook.FacebookSdk
+//import com.facebook.applinks.AppLinkData
+//import com.google.android.gms.ads.identifier.AdvertisingIdClient
+//import com.onesignal.OneSignal
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -28,61 +28,61 @@ class MainClass {
      fun initialize(context: Context, activity: Activity, af_key: String, osKey: String, fbClientToken: String, endpoint: String, media_source: String, af_status: String, campaign : String, idfa: String, timezone: String, af_id: String, deep: String, logicFun: (String) -> Unit) {
 
     //    if(isDeveloperModeEnabled(context)){
-            FacebookSdk.setClientToken(fbClientToken)
-            FacebookSdk.setAutoInitEnabled(true)
-            FacebookSdk.fullyInitialize()
-            FacebookSdk.sdkInitialize(context)
-
-            AppLinkData.fetchDeferredAppLinkData(context) {
-                if (it != null) {
-                  val delim = "://"
-                  val list = it.targetUri.toString().split(delim)
-                  deepLink = list[1];
-                }
-            }
-
-            OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
-            OneSignal.initWithContext(context)
-            OneSignal.setAppId(osKey)
-
-            val otrewqwdasd: AppsFlyerConversionListener =
-                object : AppsFlyerConversionListener {
-                    override fun onConversionDataSuccess(appsflyerConversationDatas: Map<String, Any?>) {
-
-                        var appsflyer_user_id = AppsFlyerLib.getInstance().getAppsFlyerUID(context)
-                        var google_ad_id = "";
-                        val advertisingInfomation = AdvertisingIdClient.getAdvertisingIdInfo(
-                            context
-                        )
-                        val googleAdId = advertisingInfomation?.id
-                        if (googleAdId != null) {
-                            google_ad_id = googleAdId
-                        };
-
-                        var request = "/${endpoint}?${media_source}=${appsflyerConversationDatas["media_source"]}&${af_status}=${appsflyerConversationDatas["af_status"]}&${campaign}=${appsflyerConversationDatas["campaign"]}&${idfa}=${google_ad_id}&${timezone}=${TimeZone.getDefault().id}&${af_id}=${appsflyer_user_id}&${deep}=${deepLink}"
-                        requestPartFromApps = request;
-
-                        if(domen != ""){
-                            makeLogicChanges(context, domen, logicFun)
-                        }
-
-                    }
-
-                    override fun onConversionDataFail(errorMessage: String) {
-
-                    }
-
-                    override fun onAppOpenAttribution(attributionData: Map<String, String?>) {
-
-                    }
-
-                    override fun onAttributionFailure(errorMessage: String) {
-
-                    }
-                }
-
-            AppsFlyerLib.getInstance().init(af_key, otrewqwdasd, context)
-            AppsFlyerLib.getInstance().start(context)
+//            FacebookSdk.setClientToken(fbClientToken)
+//            FacebookSdk.setAutoInitEnabled(true)
+//            FacebookSdk.fullyInitialize()
+//            FacebookSdk.sdkInitialize(context)
+//
+//            AppLinkData.fetchDeferredAppLinkData(context) {
+//                if (it != null) {
+//                  val delim = "://"
+//                  val list = it.targetUri.toString().split(delim)
+//                  deepLink = list[1];
+//                }
+//            }
+//
+//            OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+//            OneSignal.initWithContext(context)
+//            OneSignal.setAppId(osKey)
+//
+//            val otrewqwdasd: AppsFlyerConversionListener =
+//                object : AppsFlyerConversionListener {
+//                    override fun onConversionDataSuccess(appsflyerConversationDatas: Map<String, Any?>) {
+//
+//                        var appsflyer_user_id = AppsFlyerLib.getInstance().getAppsFlyerUID(context)
+//                        var google_ad_id = "";
+//                        val advertisingInfomation = AdvertisingIdClient.getAdvertisingIdInfo(
+//                            context
+//                        )
+//                        val googleAdId = advertisingInfomation?.id
+//                        if (googleAdId != null) {
+//                            google_ad_id = googleAdId
+//                        };
+//
+//                        var request = "/${endpoint}?${media_source}=${appsflyerConversationDatas["media_source"]}&${af_status}=${appsflyerConversationDatas["af_status"]}&${campaign}=${appsflyerConversationDatas["campaign"]}&${idfa}=${google_ad_id}&${timezone}=${TimeZone.getDefault().id}&${af_id}=${appsflyer_user_id}&${deep}=${deepLink}"
+//                        requestPartFromApps = request;
+//
+//                        if(domen != ""){
+//                            makeLogicChanges(context, domen, logicFun)
+//                        }
+//
+//                    }
+//
+//                    override fun onConversionDataFail(errorMessage: String) {
+//
+//                    }
+//
+//                    override fun onAppOpenAttribution(attributionData: Map<String, String?>) {
+//
+//                    }
+//
+//                    override fun onAttributionFailure(errorMessage: String) {
+//
+//                    }
+//                }
+//
+//            AppsFlyerLib.getInstance().init(af_key, otrewqwdasd, context)
+//            AppsFlyerLib.getInstance().start(context)
 //        } else {
 //            logicFun("false")
 //        }
