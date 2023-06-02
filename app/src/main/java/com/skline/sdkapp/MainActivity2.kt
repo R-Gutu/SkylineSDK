f68fc3f74fe16c6024d8ead75a8213f7e7c03837
R-Gutu <56069744+R-Gutu@users.noreply.github.com>
2023-06-02 14:39:01 +0300

    fix

--- app/src/main/java/com/skline/sdkapp/MainActivity2.kt
+++ app/src/main/java/com/skline/sdkapp/MainActivity2.kt
@@ -4,16 +4,10 @@ import android.content.Context
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.skline.sdksupport.MainClass
 
 
 class MainActivity2 : AppCompatActivity() {
-
-    lateinit var remoteConfig : FirebaseRemoteConfig
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
